037d78455c6b67eb9c89f16389ae43a1496e8396
Eduardo Navío <naviomaya@gmail.com>
2024-05-15 08:48:20 +0200

    primeros pasos portfolio

---
+++ Portfolio responsive/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "dm-sans": "'DM Sans', sans-serif"
+      },
+      colors: {
+        "green-color": "#27AE60",
+        "gray-color": "#828282",
+        "card-color": "#212121",
+        "color-background": "#171717",
+        "menu-color": "#172554"
+      },
+
+      backgroundImage: {
+        'menu-close': "url('../assets/cross.svg')",
+        'menu-burguer': "url('../assets/burguer.svg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
